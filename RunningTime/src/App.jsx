ea6f9b0c353241bbe5645261e4649346a43b062b
SGO Consultora <administracion@sgoconsultora.com>
2022-07-21 23:52:01 -0300

    se agrega widget y container

--- RunningTime/src/App.jsx
+++ RunningTime/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import ItemListContainer from './components/mainContainer/ItemListContainer'
 
 function App() {
  
-
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <ItemListContainer greeting="Hola Humano" />
     </>
   )
 }
 
-export default App
+export default App 
---
+++ RunningTime/src/components/navbar/CartWidget.jsx
@@ -0,0 +1,8 @@
+
+const CartWidget = () => {
+  return (
+    <i className="bi bi-cart-dash"> </i>
+  )
+}
+
+export default CartWidget
